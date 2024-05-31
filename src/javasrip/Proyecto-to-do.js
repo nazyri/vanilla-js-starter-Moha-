20f507e6376e7d5f9ffc3c7f6782def8a45c40f5
nazyri <mohachevezsaborio@gmail.com>
2024-05-31 15:59:32 -0600

    Inicio Del Proyecto

---
+++ src/javasrip/Proyecto-to-do.js
@@ -0,0 +1,14 @@
+let tarea = document.getElementById('input')
+let boton = document.getElementById('boton-2')
+let lista = document.getElementById('cuadro')
+let circulo = document.getElementById('circulo')
+
+
+boton.addEventListener('click', function() {
+  lista.innerHTML = tarea.value
+});
+
+boton.addEventListener('click', function() {
+   
+    
+});
