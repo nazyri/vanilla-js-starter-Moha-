b79b2698c191a36d006ef1528b358cef55d8f516
nazyri <mohachevezsaborio@gmail.com>
2024-06-04 15:46:23 -0600

    get Y Post

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 // Inserte el código aquí
-const btnVer = document.getElementById("ver")
-
+const btnVer = document.getElementById("boton")
 async function datos() {
     try {
         const respuesta = await fetch("tareas.json")
@@ -11,22 +10,61 @@ async function datos() {
     }
 }
 
-async function darDatos() {
+//GET
+async function tarea() {
     try {
-        let tarea={
-            id:Date.now(),
-            nombre:InputDeviceInfo.value
-        }
-        const respuesta = await fetch("",{
-            method: "POST",
-            headers: {
-                "content-type": "aplication/json; charset=utf-8"
-            },
-            body: JSON.stringify(tarea)
-        })
-        console.log(`La tarea ${tarea.id} fue agregada`);
+        const response = await fetch('http://localhost:3000/api/task')
+        const data = await response.json();
+        data.forEach(tareas=>{
+        let p = document.createElement("p")
+        let checkbox = document.createElement("input")
+        checkbox.type = "checkbox"
+        let div = document.createElement("div")
+        let cuadro = document.createElement("cuadro")
+
+        p.innerHTML = tareas.nombre
+
+        p.appendChild(checkbox)
+        checkbox.appendChild(div)
+        div.appendChild(cuadro)
+        cuadro.appendChild(p)
+
+       })
     } catch (error) {
         console.error(error);
     }
 }
-btnVer.addEventListener("click", datos)
+
+//POST
+async function darDatos() {
+    try {
+        let tarea={
+            id:Date.now(),
+            nombre:input.value
+        }
+        const respuesta = await fetch('http://localhost:3000/api/task',{
+            method: "POST",
+            headers: {
+                "Content-type": "aplication/json; charset=UTF-8"
+            },
+            body: JSON.stringify(tarea)
+        })
+
+
+        let registro = await respuesta.json()
+        console.log(registro);
+        tarea()
+
+        console.log(`La tarea ${tarea.id} fue agregada`);
+    } catch (error) {
+        console.error(error);
+    }  
+}
+
+btnVer.addEventListener("click",()=>{
+   darDatos()
+})
+
+
+
+
--- src/javasrip/Proyecto-to-do.js
+++ src/javasrip/Proyecto-to-do.js
@@ -1,16 +1,1 @@
-let tarea = document.getElementById('input')
-let agregar = document.getElementById('boton-2')
-let lista = document.getElementById('cuadro')
-let circulo = document.getElementById('contador')
-let check = document.getElementById('check1')
 
-agregar.addEventListener('click', function () {
-  lista.innerHTML = tarea.value
-  circulo.innerHTML =
-    parseInt(contador.innerHTML) + 1
-  alert("Agregue el nombre de la tarea")
-});
-
-let arreglo = [
- 
-]
